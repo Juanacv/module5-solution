4e30dfcc8bb08d2f86496c98f12f75a7687d3913
Juanacv <juan.acv@gmail.com>
2017-01-22 21:47:35 +0100

    First Commit

---
+++ src/public/sign-up/sign-up.controller.js
@@ -0,0 +1,27 @@
+(function() {
+  'use strict';
+
+  angular.module('public')
+  .controller('SignUpController',SignUpController);
+
+  SignUpController.$inject = ['MenuService'];
+  function SignUpController(MenuService) {
+    var signUpCtrl = this;
+    signUpCtrl.result = "";
+    signUpCtrl.submit = function() {
+      MenuService.getDish(signUpCtrl.myInfo.favoriteDish).then(function(response) {
+        if (response === false) {
+          signUpCtrl.result = "No such menu number exist";
+        }
+        else {
+          signUpCtrl.myInfo.title = response.name;
+          signUpCtrl.myInfo.description = response.description;
+          signUpCtrl.result = "Your information has been saved";
+          MenuService.setMyInfo(signUpCtrl.myInfo);
+        }
+      });
+
+    }
+  }
+
+})();
